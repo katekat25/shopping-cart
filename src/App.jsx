9e8d7b05bc6d15a7e69f61ea39d04f9f27d41923
Kate Schumacher <kate.schumacher@gmail.com>
2025-06-12 08:43:10 -0500

    Create mockup of main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="navbar">
+          <p>CoolStuff.com</p>
+          <div className="button-container">
+            <button>Profile</button>
+            <button>Search</button>
+            <button>My cart</button>
+          </div>
+        </div>
+        <div>
+          <h1>CoolStuff.com</h1>
+          <p>We bring you the coolest stuff, all the time. Shop now, or be a poser. It's your choice.</p>
+        </div>
+        <footer>
+          <p>Brought to you by Kate Schumacher</p>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
