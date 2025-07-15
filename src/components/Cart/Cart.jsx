e92086fe0812f487d81b5b4609fae7d0534c37af
Kate Schumacher <kate.schumacher@gmail.com>
2025-07-15 06:12:36 -0500

    Add shop link to empty cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,22 @@
 import { useContext } from "react";
 import { ShopContext } from "../../App";
 import styles from "./Cart.module.css"
+import { Link } from "react-router-dom";
 
 const Cart = () => {
     const { cartItems, removeFromCart } = useContext(ShopContext);
     return <>
         <div className={styles.container}>
-            {cartItems.map((item) => (
-                <div>
-                    <h3>{item.title}</h3>
-                    <button onClick={() => removeFromCart(item)}>Remove item</button>
-                </div>
-            ))}
+            {cartItems.length === 0 ? (
+                <p>Your cart is empty. <Link to="/shop">Shop now</Link></p>
+            ) : (
+                cartItems.map((item) => (
+                    <div>
+                        <h3>{item.title}</h3>
+                        <button onClick={() => removeFromCart(item)}>Remove item</button>
+                    </div>
+                ))
+            )}
         </div>
     </>
 }
