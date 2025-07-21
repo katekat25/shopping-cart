0cc54b099f4078f2a8e3972b1adbf5427652de64
Kate Schumacher <kate.schumacher@gmail.com>
2025-07-21 01:24:15 -0500

    Make quantity modifier its own module

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,33 +1,13 @@
 import styles from './ItemCard.module.css'
-import { useState } from 'react';
+import Quantity from '../Quantity/Quantity';
 
 const ItemCard = (props) => {
-    const [quantity, setQuantity] = useState(1);
-
-    const handleAddToCart = (e) => {
-        e.preventDefault();
-        props.addToCart(props.product, Number(quantity));
-    }
-
     return <div className={styles.card}>
         <img src={props.product.image} alt={props.product.title} />
         <div className={styles.textOverlay}>
             <h3>{props.product.title}</h3>
             <p>${props.product.price}</p>
-            <form onSubmit={handleAddToCart}>
-                <div className={styles.buttonContainer}>
-                    <button type="button" onClick={() => setQuantity(prev => Math.max(1, Number(prev) - 1))}>-</button>
-                    <input
-                        type="number"
-                        min="1"
-                        value={quantity}
-                        onChange={(e) => setQuantity(e.target.value)}
-                        className={styles.quantityInput}
-                    />
-                    <button type="button" onClick={() => setQuantity(prev => Number(prev) + 1)}>+</button>
-                    <button type="submit">Add to cart</button>
-                </div>
-            </form>
+            <Quantity product={props.product} addToCart={props.addToCart}></Quantity>
         </div>
     </div>
 }
---
+++ src/components/Quantity/Quantity.jsx
@@ -0,0 +1,30 @@
+import styles from './Quantity.module.css'
+import { useState } from 'react';
+
+const Quantity = (props) => {
+    const [quantity, setQuantity] = useState(1);
+
+    const handleAddToCart = (e) => {
+        e.preventDefault();
+        props.addToCart(props.product, Number(quantity));
+    }
+
+    return <>
+        <form onSubmit={handleAddToCart}>
+            <div className={styles.buttonContainer}>
+                <button type="button" onClick={() => setQuantity(prev => Math.max(1, Number(prev) - 1))}>-</button>
+                <input
+                    type="number"
+                    min="1"
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)}
+                    className={styles.quantityInput}
+                />
+                <button type="button" onClick={() => setQuantity(prev => Number(prev) + 1)}>+</button>
+                <button type="submit">Add to cart</button>
+            </div>
+        </form>
+    </>
+}
+
+export default Quantity;
