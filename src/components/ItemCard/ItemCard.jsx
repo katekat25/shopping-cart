f4a98fd0a241c661f850d356dbc7743d597cbe58
Kate Schumacher <kate.schumacher@gmail.com>
2025-07-22 19:15:58 -0500

    Add hover transition to item cards

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -5,8 +5,8 @@ const ItemCard = (props) => {
     return <div className={styles.card}>
         <img src={props.product.image} alt={props.product.title} />
         <div className={styles.textOverlay}>
-            <h3>{props.product.title}</h3>
-            <p>${props.product.price}</p>
+            <h3 className={styles.itemName}>{props.product.title}</h3>
+            <p className={styles.itemPrice}>${props.product.price}</p>
             <Quantity product={props.product} addToCart={props.addToCart}></Quantity>
         </div>
     </div>
