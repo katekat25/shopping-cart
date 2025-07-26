ace2526560e47314f9eabf10f080130577286af5
Kate Schumacher <kate.schumacher@gmail.com>
2025-07-26 00:06:27 -0500

    Style quantity component

--- src/components/Quantity/Quantity.jsx
+++ src/components/Quantity/Quantity.jsx
@@ -12,15 +12,17 @@ const Quantity = (props) => {
     return <>
         <form onSubmit={handleAddToCart}>
             <div className={styles.buttonContainer}>
-                <button type="button" onClick={() => setQuantity(prev => Math.max(1, Number(prev) - 1))}>-</button>
-                <input
-                    type="number"
-                    min="1"
-                    value={quantity}
-                    onChange={(e) => setQuantity(e.target.value)}
-                    className={styles.quantityInput}
-                />
-                <button type="button" onClick={() => setQuantity(prev => Number(prev) + 1)}>+</button>
+                <div className={styles.quantity}>
+                    <button type="button" className={styles.leftButton} onClick={() => setQuantity(prev => Math.max(1, Number(prev) - 1))}>-</button>
+                    <input
+                        type="number"
+                        min="1"
+                        value={quantity}
+                        onChange={(e) => setQuantity(e.target.value)}
+                        className={styles.quantityInput}
+                    />
+                    <button type="button" className={styles.rightButton} onClick={() => setQuantity(prev => Number(prev) + 1)}>+</button>
+                </div>
                 <button type="submit">Add to cart</button>
             </div>
         </form>
