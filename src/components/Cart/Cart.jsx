eebfd0b82b58af84d3fa19825079690838ab1395
Kate Schumacher <kate.schumacher@gmail.com>
2025-07-29 00:27:03 -0500

    Create skeleton layout of cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -12,32 +12,45 @@ const Cart = () => {
     }, 0);
 
     return (
-        <div className={styles.container}>
+        <div>
             {cartItems.length === 0 ? (
                 <p>Your cart is empty. <Link to="/shop">Shop now</Link></p>
             ) : (
-                <>
-                    {cartItems.map((item) => (
-                        <div key={item.id}>
-                            <h3>{item.title}</h3>
-                            <Quantity
-                                addToCart={addToCart}
-                                product={item}
-                                quantity={item.quantity}
-                            />
-                            <h3>
-                                ${(
-                                    item.quantity * Number(item.price.replace(/,/g, ""))
-                                ).toLocaleString("en", { minimumFractionDigits: 2 })}
-                            </h3>
-                            <button onClick={() => removeFromCart(item)}>Remove item</button>
-                        </div>
-                    ))}
-                    <hr />
-                    <h2>
-                        Total: ${total.toLocaleString("en", { minimumFractionDigits: 2 })}
-                    </h2>
-                </>
+                <div className={styles.container}>
+                    <div className={styles.shippingInfo}>
+                        <h2>Shipping information</h2>
+                        <form>
+                            <input type="text"></input>
+                            <input type="email"></input>
+                            <input type="text"></input>
+                            <input type="number"></input>
+                        </form>
+                    </div>
+                    <div className={styles.orderSummary}>
+                        <h2>Order Summary</h2>
+                        {cartItems.map((item) => (
+                            <div key={item.id}>
+                                <h3>{item.title}</h3>
+                                <Quantity
+                                    addToCart={addToCart}
+                                    product={item}
+                                    quantity={item.quantity}
+                                />
+                                <h3>x{item.quantity}</h3>
+                                <h3>
+                                    ${(
+                                        item.quantity * Number(item.price.replace(/,/g, ""))
+                                    ).toLocaleString("en", { minimumFractionDigits: 2 })}
+                                </h3>
+                                <button onClick={() => removeFromCart(item)}>Remove item</button>
+                            </div>
+                        ))}
+                        <hr />
+                        <h2>
+                            Total: ${total.toLocaleString("en", { minimumFractionDigits: 2 })}
+                        </h2>
+                    </div>
+                </div>
             )}
         </div>
     );
