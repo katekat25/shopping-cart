72a6132e2af4d6053d665f833c824d2a5cc61ec9
Kate Schumacher <kate.schumacher@gmail.com>
2025-07-29 00:52:40 -0500

    Move quantity indicator below cards

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,18 +2,22 @@ import styles from './ItemCard.module.css'
 import Quantity from '../Quantity/Quantity';
 
 const ItemCard = (props) => {
-    return <div className={styles.card}>
-        <img src={props.product.image} alt={props.product.title} />
-        <div className={styles.textOverlay}>
-            <div className={styles.detailsOverlay}>
-                <h3 className={styles.itemName}>{props.product.title}</h3>
-                <p className={styles.itemPrice}>${props.product.price}</p>
-            </div>
-            <div className={styles.quantity}>
+    return <>
+        <div>
+            <div className={styles.card}>
+                <img src={props.product.image} alt={props.product.title} />
+                <div className={styles.textOverlay}>
+                    <div className={styles.detailsOverlay}>
+                        <h3 className={styles.itemName}>{props.product.title}</h3>
+                        <p className={styles.itemPrice}>${props.product.price}</p>
+                    </div>
+                    <div className={styles.quantity}>
+                    </div>
+                </div>
+            </div >
             <Quantity product={props.product} addToCart={props.addToCart}></Quantity>
-            </div>
         </div>
-    </div >
+    </>
 }
 
 export default ItemCard;
